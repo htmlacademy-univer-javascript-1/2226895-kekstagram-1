35803be495bead80e92152702a70c1f96603871e
Ekaterina Sergeeva <katy.o.sergeeva@gmail.com>
2022-09-23 19:35:37 +0300

    two functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 function getRandom(min, max) {
-    if (min > max || min < 0 || max < 0) {
-        return 'Неверный диапазон';
-    } else {
-        return Math.floor(Math.random() * (max + 1 - min) + min);
-    }
+  if (min > max || min < 0 || max < 0) {
+    return 'Неверный диапазон';
+  } else {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
   }
+}
 
-  function stringLength(str, maxLength) {
-    if (str.length <= maxLength) {
-        return true;
-    } else { 
-        return false;
-    }
+function stringLength(str, maxLength) {
+  if (str.length <= maxLength) {
+    return true;
+  } else { 
+    return false;
   }
+}
+
+console.log(getRandom(2, 8));
+console.log(stringLength('erty', 7));
