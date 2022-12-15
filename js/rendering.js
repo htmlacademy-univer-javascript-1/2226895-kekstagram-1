2e96675a832daf7f71c650f40bdd92d561df4159
Ekaterina Sergeeva <katy.o.sergeeva@gmail.com>
2022-12-15 20:25:27 +0300

    module12-1

--- js/rendering.js
+++ js/rendering.js
@@ -1,13 +1,25 @@
 import {showFullSize} from './fullSize.js';
+import {getData} from './api.js';
+import {initFilterButtons} from './filters.js';
+import {showAlert} from './util.js';
 
 const photosTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const listFragment = document.createDocumentFragment();
 const photosList = document.querySelector('.pictures');
+const imgFilters = document.querySelector('.img-filters');
+
+const removeOLdPictureList = () => {
+  photosList.querySelectorAll('.picture').forEach((item) => item.remove());
+};
+
 export const renderMin = (photos) => {
+  removeOLdPictureList();
   if (photos !== undefined) {
     photos.forEach((element) => {
       const onePhoto = photosTemplate.cloneNode(true);
+      onePhoto.href = `#${element.id}`;
       onePhoto.querySelector('.picture__img').src = element.url;
+      onePhoto.querySelector('.picture__img').alt = element.description;
       onePhoto.querySelector('.picture__likes').textContent = element.likes;
       onePhoto.querySelector('.picture__comments').textContent = element.comments.length;
       listFragment.appendChild(onePhoto);
@@ -20,6 +32,18 @@ export const renderMin = (photos) => {
   }
 };
 
-renderMin();
+const bigPicElement = document.querySelector('.big-picture');
 
+export const getPictureList = () => {
+  getData((data) => {
+    renderMin(data);
+    initFilterButtons(data);
+    bigPicElement.classList.add('hidden');
+    imgFilters.classList.remove('img-filters--inactive');
+
+  }, () => showAlert('Фотографии отсутствуют...'));
+};
+
+renderMin();
+getPictureList();
 
